ab621473845eded0ca105f088b3f6ad17d8950f2
Pallak Anand <PallakAnan@172-16-12-187.DYNAPOOL.NYU.EDU>
2017-04-03 15:53:01 -0400

    first commit

---
+++ app/src/main/java/com/example/pallakanan/myapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pallakanan.myapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+        TextView textView = (TextView) findViewById(R.id.textView);
+    }
+}
